06fb37a10347b8ae1945b120608fa2ab9d22f417
Douglas Tertuliano <douglasjtds@gmail.com>
2019-04-26 19:57:37 -0300

    creating project

---
+++ Console_AirplaneSimulator/Console_AirplaneSimulator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Console_AirplaneSimulator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
