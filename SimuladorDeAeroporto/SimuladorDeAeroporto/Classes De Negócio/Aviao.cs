e90c6ed4eaf3ce53fb36d86de8e63199e6bc700b
Douglas Tertuliano <douglasjtds@gmail.com>
2019-04-28 16:04:39 -0300

    novas classes

---
+++ SimuladorDeAeroporto/SimuladorDeAeroporto/Classes De Negócio/Aviao.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimuladorDeAeroporto.Classes_De_Negócio
+{
+    public class Aviao
+    {
+        public int Id_Aviao;
+
+        public int NivelGasolina; //quantidade de unidades de tempo que pode permanecer no ar (de 1 a 20)
+    }
+}
