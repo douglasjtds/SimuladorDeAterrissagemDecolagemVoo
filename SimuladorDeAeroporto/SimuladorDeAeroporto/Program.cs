91c3242005ab04a7493c1dc99e3b5cf9ea4c9714
Douglas Tertuliano <douglasjtds@gmail.com>
2019-04-28 13:18:50 -0300

    alterando projeto para .NET Framework +

---
+++ SimuladorDeAeroporto/SimuladorDeAeroporto/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SimuladorDeAeroporto
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("----- [Simulador de Aeroporto] -----");
+            Thread.Sleep(1000);
+        } 
+    }
+}
