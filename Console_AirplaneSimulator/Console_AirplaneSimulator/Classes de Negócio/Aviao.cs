bac1f839608f4fe0b743af9db9a794f1b79da96c
Douglas Tertuliano <douglasjtds@gmail.com>
2019-04-28 12:50:37 -0300

    just adding some files

---
+++ Console_AirplaneSimulator/Console_AirplaneSimulator/Classes de Negócio/Aviao.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Console_AirplaneSimulator.Classes_de_Negócio
+{
+    public class Aviao
+    {
+    }
+}
---
+++ Console_AirplaneSimulator/Console_AirplaneSimulator/Interfaces/IAviao.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Console_AirplaneSimulator.Interfaces
+{
+    public interface IAviao
+    {
+
+    }
+}
