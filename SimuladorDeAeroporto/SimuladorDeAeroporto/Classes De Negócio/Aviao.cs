48d081ed818922be681a1e8693df222e90f4ba38
Douglas Tertuliano <douglasjtds@gmail.com>
2019-04-28 21:38:40 -0300

    adicionando e implementando interface de Fila

--- SimuladorDeAeroporto/SimuladorDeAeroporto/Classes De Negócio/Aviao.cs
+++ SimuladorDeAeroporto/SimuladorDeAeroporto/Classes De Negócio/Aviao.cs
@@ -11,5 +11,18 @@ namespace SimuladorDeAeroporto.Classes_De_Negócio
         public int Id_Aviao;
 
         public int NivelGasolina; //quantidade de unidades de tempo que pode permanecer no ar (de 1 a 20)
+
+        public int ReferenteAPista;
+
+        /// <summary>
+        /// Referente ao Id_Aviao: números pares são aviões decolando, números ímpares são aviões pousando.
+        /// </summary>
+        /// <returns></returns>
+        public bool IsAviaoDecolando()
+        {
+            if (Id_Aviao % 2 == 0)
+                return true;
+            return false;
+        }
     }
 }
